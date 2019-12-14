239d0eb0241c2ea76125bcfb1d21c1a91925bbc6
khaitruong922 <56820749+khaitruong922@users.noreply.github.com>
2019-12-14 19:30:54 +0700

    new stuff

---
+++ Adventurer - Weapon Master Season 1/Assets/Script/Gameplay/Enemy/Lava.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    public float damagePerSec = 20f;
+    public float delay = 1f;
+    public float duration = 4f;
+    private void Start(){
+        Destroy(gameObject,duration);
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            other.GetComponent<Player>().TakeDamage(damagePerSec*Time.deltaTime);
+        }
+    } 
+}
---
+++ Adventurer - Weapon Master Season 1/Assets/Script/Gameplay/Enemy/MageVoidling.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageVoidling : MonoBehaviour
+{
+    public float range = 10f;
+    public float timeBtwCast=6f;
+    private float timeforNextCast;
+
+    private Transform target;
+    public GameObject spell; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<Player>().GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null && Vector2.Distance(transform.position, target.position) <= range)
+        {
+            if(timeforNextCast<=0)
+            {
+                Instantiate(spell, target.position,Quaternion.identity);
+                timeforNextCast=timeBtwCast;
+            }
+            else 
+            {
+                timeforNextCast -= Time.deltaTime;
+            }
+        }
+    } 
+} 
