355172f30ecb3817b51809c1f6688d296bb4727c
khaitruong922 <56820749+khaitruong922@users.noreply.github.com>
2019-12-17 01:09:13 +0700

    Difficult adjust

--- Adventurer - Weapon Master Season 1/Assets/Script/Gameplay/Enemy/Enemy.cs
+++ Adventurer - Weapon Master Season 1/Assets/Script/Gameplay/Enemy/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
 {
     public float maxHP = 100f;
     public float currentHP;
+    public float dropChance = 0.5f;
     public GameObject deathEffect;
     public GameObject healthBar; //access to the health bar
     public GameObject healthPack;
@@ -37,9 +38,11 @@ public class Enemy : MonoBehaviour
         Debug.Log("Enemy left: " + count);
         Destroy(gameObject);
         //death effect
+        if (Random.Range(0.0f,1.0f) <= dropChance){
+        Instantiate(healthPack,transform.position,Quaternion.identity);
+        }
         GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(effect,0.1f);
-        Instantiate(healthPack,transform.position,Quaternion.identity);
         }
     public float PercentHP(){
         return(float) currentHP/maxHP;
