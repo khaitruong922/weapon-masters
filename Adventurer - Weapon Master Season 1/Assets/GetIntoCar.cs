712bb2c348815a6757db2d8cd1fb359d865b78b7
khaitruong922 <56820749+khaitruong922@users.noreply.github.com>
2019-12-17 12:21:43 +0700

    car

---
+++ Adventurer - Weapon Master Season 1/Assets/GetIntoCar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetIntoCar : MonoBehaviour
+{
+private Transform cart;
+private Transform player;
+void Start(){
+    player = FindObjectOfType<Player>().GetComponent<Transform>();
+    cart = GetComponent<Transform>();
+}
+void Update(){
+    cart.position = player.position + new Vector3(0,-1,0);
+}
+}
