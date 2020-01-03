911a401cd6841eb047d657fa4ec6bf7715800d07
khai <kd.kidr@gmail.com>
2020-01-03 12:03:24 +0700

    assassin in progress

--- Adventurer - Weapon Master Season 1/Assets/Script/Gameplay/Enemy/AssassinBoss.cs
+++ Adventurer - Weapon Master Season 1/Assets/Script/Gameplay/Enemy/AssassinBoss.cs
@@ -6,6 +6,7 @@ public class AssassinBoss : MonoBehaviour
 {
     [Header("Object references")]
     private Rigidbody2D rb;
+    private Animator anim;
     private EnemyRotation enemy;
     private SpriteRenderer spriteRenderer;
     public LayerMask playerOnly;
@@ -15,16 +16,17 @@ public class AssassinBoss : MonoBehaviour
     public float damage = 60f;
     public float attackRadius = 1.2f;
     public float attackSpeed = 2.5f;
+
     [Header("Bleeding Edge")]
-    public float damagePerTick=10f;
-    public int numberOfTicks=4;
-    public float frequency=0.5f;
+    public float damagePerTick = 10f;
+    public int numberOfTicks = 4;
+    public float frequency = 0.5f;
 
 
     [Header("Arc Stars")]
     public GameObject shuriken;
     public float qCooldown = 5f;
-    public float projectileSpreadVaule = 0.1f;
+    public float projectileSpreadVaule = 0.2f;
     public float projectileForce = 30f;
 
     [Header("Dash")]
@@ -43,12 +45,13 @@ public class AssassinBoss : MonoBehaviour
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
         player = FindObjectOfType<Player>().GetComponent<Transform>();
         enemy = GetComponent<EnemyRotation>();
         InvokeRepeating("Attack", 1f, 1 / attackSpeed);
-        InvokeRepeating("BlackHole", qCooldown, qCooldown);
-        InvokeRepeating("Shield", eCooldown, eCooldown);
-        InvokeRepeating("Explosion", rCooldown, rCooldown);
+        InvokeRepeating("SpreadProjectile", qCooldown, qCooldown);
+        InvokeRepeating("Dash", eCooldown, eCooldown);
+        //InvokeRepeating("LethalAttack", rCooldown, rCooldown);
     }
     private void Shoot(float x, float y, GameObject projectile)
     {
@@ -56,17 +59,28 @@ public class AssassinBoss : MonoBehaviour
         Rigidbody2D rb = go.GetComponent<Rigidbody2D>();
         rb.AddForce(attackPosition.TransformVector(x, y, 0) * projectileForce, ForceMode2D.Impulse);
     }
-    private IEnumerator MultiShoot()
+    private void Dash(){
+        dashTimeLeft=dashTime;
+    }
+    private void FixedUpdate()
     {
-        for (int i = 0; i < Random.Range(1, 4); i++)
+        
+        if (dashTimeLeft > 0)
         {
-            Shoot(0, 1, shuriken);
-            AudioManager.Instance.Play("MageAttack");
-            yield return new WaitForSeconds(1);
+            dashTimeLeft-=Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, player.position, dashSpeed * Time.deltaTime);
+        }
+    }
+    private void SpreadProjectile()
+    {
+        for (int i = -5; i < 6; i++)
+        {
+            Shoot(i, 1, shuriken);
         }
     }
     private void Attack()
     {
+        anim.SetTrigger("Attack");
         Collider2D[] hitZone = Physics2D.OverlapCircleAll(attackPosition.position, attackRadius, playerOnly);
         for (int i = 0; i < hitZone.Length; i++)
         {
